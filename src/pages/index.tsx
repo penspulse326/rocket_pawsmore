0f94e96c875484542c044465a362e6f20b75d769
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-01-15 17:39:36 +0800

    test: excute upload well with .env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import Upload from "@/components/Upload";
+
 export default function Home() {
-  return <main>Pawsmore</main>;
+  return (
+    <main className="flex flex-col items-center justify-between p-24 min-h-screen bg-slate-400">
+      <Upload />
+    </main>
+  );
 }
