ea1df0e0c2fc94d80a829c475b2b24754917d430
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-01-23 17:24:13 +0800

    style: user's pet card layout

--- src/components/SocialSideBarLeft.tsx
+++ src/components/SocialSideBarLeft.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function SocialSideBarLeft() {
-  return <div className="col-span-3 h-full bg-slate-300">我是左選單</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <aside className="col-span-3 flex flex-col pt-8 h-full">
+      {/* 寵物列表 */}
+      <div className="flex-grow">
+        {/* 寵物卡片 */}
+        <div className="flex flex-col gap-4 mx-auto p-4 max-w-[204px] border border-stroke rounded-[30px] bg-white">
+          <Image
+            src="/test/photo-cat-test.png"
+            width={172}
+            height={172}
+            priority
+            alt="寵物照片"
+            className="rounded-[30px] object-cover"
+          />
+          <div>
+            <p>角龍寶寶</p>
+            <p>@littleprincess126</p>
+          </div>
+          <Link
+            href="#"
+            className="mb-4 py-2 rounded-[30px] bg-primary text-white text-center hover:bg-primary/70 duration-300"
+          >
+            寵物檔案
+          </Link>
+        </div>
+      </div>
+    </aside>
+  );
 }
