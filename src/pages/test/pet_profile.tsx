9d65d8a643ded5ecc86e9c10d2843d75da8642d6
Finn Mao <loverainys@gmail.com>
2024-01-24 02:13:58 +0800

    chore: add Footer component

--- src/pages/test/pet_profile.tsx
+++ src/pages/test/pet_profile.tsx
@@ -1,7 +1,18 @@
-export default function Social() {
+import Footer from "@/components/petProfile/Footer";
+
+const ProfileCard = () => {
+  return <section>Card</section>;
+};
+const ProfileGallery = () => {
+  return <section>Gallery</section>;
+};
+
+export default function PetProfile() {
   return (
-    <main className="flex flex-col items-center justify-between p-24 min-h-screen bg-slate-400">
-      我是寵物檔案頁面
+    <main className="flex flex-col gap-y-12 items-center mt-[64px]">
+      <ProfileCard />
+      <ProfileGallery />
+      <Footer />
     </main>
   );
 }
