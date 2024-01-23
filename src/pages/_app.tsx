43f3ef3dfb253bef39c28f1141487dd4c9982f40
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-01-23 14:37:06 +0800

    style: add fonts to global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import "@/styles/globals.css";
 import "@/styles/animations.css";
 import type { AppProps } from "next/app";
+import { Noto_Sans_TC } from "next/font/google";
+
+const notos = Noto_Sans_TC({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notos.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
