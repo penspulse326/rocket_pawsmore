d2e4906879d18eda873613f455838029f5393b0c
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-01-23 10:24:26 +0800

    style: set navbar basic layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <nav className="outter">我是導覽列</nav>;
+  return (
+    <nav className="outter flex justify-between items-center h-16">
+      <Link href="/test/social">Pawsmore</Link>
+      <div>
+        <Link href="/test/social">社群首頁</Link>
+        <Link href="/test/social">數據紀錄</Link>
+      </div>
+    </nav>
+  );
 }
