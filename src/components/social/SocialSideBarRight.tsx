0c2b7a1262d807362cc2595559f6fd0b97da2a72
SHIN <akiyachiumi@gmail.com>
2024-01-24 08:36:01 +0800

    style: add search bar in right sidebar

--- src/components/social/SocialSideBarRight.tsx
+++ src/components/social/SocialSideBarRight.tsx
@@ -1,3 +1,16 @@
+import { IconSearch } from "@tabler/icons-react";
+
 export default function SocialSideBarRight() {
-  return <div className="col-span-3 bg-slate-300">我是右選單</div>;
+  return (
+    <div className="col-span-3 bg-white">
+      <section className="flex items-center gap-2 mt-8 px-8 py-4 rounded-full bg-stroke">
+        <input
+          type="text"
+          placeholder="搜尋名稱、分類⋯⋯"
+          className="w-full outline-none bg-transparent"
+        />
+        <IconSearch size={24} />
+      </section>
+    </div>
+  );
 }
