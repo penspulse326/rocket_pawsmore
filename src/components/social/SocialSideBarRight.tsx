e68f378c8de86b3423cf0b3f4c9acb73a898ea34
SHIN <akiyachiumi@gmail.com>
2024-01-24 08:58:51 +0800

    style: add section-explore

--- src/components/social/SocialSideBarRight.tsx
+++ src/components/social/SocialSideBarRight.tsx
@@ -1,4 +1,6 @@
 import { IconSearch } from "@tabler/icons-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function SocialSideBarRight() {
   return (
@@ -10,6 +12,102 @@ export default function SocialSideBarRight() {
           className="w-full outline-none bg-transparent"
         />
         <IconSearch size={24} />
+      </section>
+      <section className="mt-8">
+        <h2 className="text-note">為您推薦</h2>
+        <ul className="flex flex-col gap-6 mt-4">
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-4">
+              <Link href="#">
+                <Image
+                  src="/test/photo-cat-test-2.png"
+                  width={48}
+                  height={48}
+                  alt="user-photo"
+                  className="rounded-full"
+                />
+              </Link>
+              <Link href="#">
+                <p>黑角龍</p>
+                <p>@blk_diablos</p>
+              </Link>
+            </div>
+            <button
+              type="button"
+              className="px-8 py-2 rounded-[30px] bg-primary text-white"
+            >
+              追蹤
+            </button>
+          </li>
+          <li className="flex justify-between items-center">
+            <div className="flex items-center gap-4">
+              <Link href="#">
+                <Image
+                  src="/test/photo-dog-test-2.png"
+                  width={48}
+                  height={48}
+                  alt="user-photo"
+                  className="rounded-full"
+                />
+              </Link>
+              <Link href="#">
+                <p>陳嘟嘟</p>
+                <p>@duduchen123</p>
+              </Link>
+            </div>
+            <button
+              type="button"
+              className="px-8 py-2 rounded-[30px] bg-primary text-white"
+            >
+              追蹤
+            </button>
+          </li>
+        </ul>
+        <h2 className="mt-8 text-note">探索</h2>
+        <div className="mt-4 px-5 text-center">
+          <div className="flex gap-4">
+            <Link href="#">
+              <Image
+                src="/test/photo-dog-test-1.png"
+                width={80}
+                height={80}
+                alt="explore dog"
+                className="mb-2 rounded-full"
+              />
+              <span>狗</span>
+            </Link>
+            <Link href="#">
+              <Image
+                src="/test/photo-cat-test.png"
+                width={80}
+                height={80}
+                alt="explore cat"
+                className="mb-2 rounded-full"
+              />
+              <span>貓</span>
+            </Link>
+            <Link href="#">
+              <Image
+                src="/test/photo-rice-test.png"
+                width={80}
+                height={80}
+                alt="explore cat"
+                className="mb-2 rounded-full"
+              />
+              <span>倉鼠</span>
+            </Link>
+          </div>
+          <Link href="#" className="flex flex-col items-center mt-4">
+            <Image
+              src="/test/photo-more.png"
+              width={188}
+              height={80}
+              alt="explore cat"
+              className="mb-2"
+            />
+            <span>更多</span>
+          </Link>
+        </div>
       </section>
     </div>
   );
