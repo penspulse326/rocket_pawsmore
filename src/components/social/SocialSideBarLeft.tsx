5ac07d00e2c44928c66d0b96b7f1398d7f450a8f
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-01-25 13:35:45 +0800

    chore: install swiper

--- src/components/social/SocialSideBarLeft.tsx
+++ src/components/social/SocialSideBarLeft.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { IconMenu2 } from "@tabler/icons-react";
+
 import PetCards from "./PetCards";
 
 export default function SocialSideBarLeft() {
