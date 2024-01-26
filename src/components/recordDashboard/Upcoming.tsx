3bd96b542a4841e5e6fa27c698198cc8b66420c8
Finn Mao <loverainys@gmail.com>
2024-01-27 00:23:30 +0800

    style: add upcoming events card

--- src/components/recordDashboard/Upcoming.tsx
+++ src/components/recordDashboard/Upcoming.tsx
@@ -1,5 +1,45 @@
+import Image from "next/image";
+
 const Upcoming = () => {
-  return <div>即將到來</div>;
+  return (
+    <section className="flex flex-col gap-y-2">
+      <div className="text-note">即將到來</div>
+      <div className="flex border border-stroke rounded-[30px] p-8">
+        {/* reminders */}
+        <ul className="flex flex-col gap-y-2 w-1/2">
+          <ol className="flex gap-x-1 items-center">
+            <Image
+              src="/test/icon-exclamation.svg"
+              width={6}
+              height={24}
+              alt="exclamation mark"
+            />
+            <li>醫療提醒</li>
+          </ol>
+          <ol className="flex flex-col gap-y-1">
+            <li>1/25 年度健檢</li>
+            <li>1/25 打疫苗</li>
+          </ol>
+        </ul>
+        {/* moments */}
+        <ul className="flex flex-col gap-y-2 w-1/2">
+          <ol className="flex gap-x-1 items-center">
+            <Image
+              src="/test/icon-dot.svg"
+              width={6}
+              height={24}
+              alt="dot symbol"
+            />
+            <li>重要時刻</li>
+          </ol>
+          <ol className="flex flex-col gap-y-1">
+            <li>1/11 初次相遇</li>
+            <li>1/17 生日快樂</li>
+          </ol>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Upcoming;
--- src/pages/test/record_dashboard.tsx
+++ src/pages/test/record_dashboard.tsx
@@ -6,7 +6,7 @@ import RecordCard from "@/components/recordDashboard/RecordCard";
 export default function RecordDashboard() {
   return (
     <section className="inner mt-[88px] flex justify-between">
-      <div className="flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-4 max-w-[896px] w-full">
         <Calendar />
         <Upcoming />
       </div>
