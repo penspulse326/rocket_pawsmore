faadad159b293fdacb8e579ef3ac6a828b01d0b5
SHIN <akiyachiumi@gmail.com>
2024-01-28 15:52:08 +0800

    style: upload view layout complete

--- src/components/social/UploadView.tsx
+++ src/components/social/UploadView.tsx
@@ -1,4 +1,10 @@
-import { IconMedal, IconX } from "@tabler/icons-react";
+import {
+  IconChevronDown,
+  IconMedal,
+  IconMovie,
+  IconPhoto,
+  IconX,
+} from "@tabler/icons-react";
 
 import Image from "next/image";
 
@@ -12,19 +18,32 @@ export default function UploadView() {
         </button>
       </div>
       <section className="flex gap-8 mt-4">
-        <label className="w-[476px] h-[476px] border border-stroke rounded-[30px]"></label>
+        <label className="flex justify-center items-center gap-8 w-[476px] border border-stroke rounded-[30px] overflow-hidden cursor-pointer">
+          <input type="file" name="media" className="hidden" />
+          <div className="flex items-center">
+            <IconPhoto size={24} />
+            <span className="ml-2 text-note">附上照片</span>
+          </div>
+          <div className="flex items-center">
+            <IconMovie size={24} />
+            <span className="ml-2 text-note">附上影片</span>
+          </div>
+        </label>
         <section className="w-[469px]">
-          <textarea className="w-full h-[259px] border border-stroke rounded-[30px]"></textarea>
+          <textarea className="scrollbar-none p-8 w-full h-[259px] border border-stroke rounded-[30px]"></textarea>
           <div className="flex gap-8 mt-8">
             <button
               type="button"
-              className="flex-grow flex flex-col items-center gap-4 py-8 max-w-[187px] border border-stroke rounded-[30px]"
+              className="flex-grow py-8 border border-stroke rounded-[30px] text-center"
             >
-              <IconMedal size={48} />
-              <span className="text-note">加上里程碑</span>
+              <IconMedal size={48} className="mx-auto" />
+              <span className="block mt-4 text-note">加上里程碑</span>
             </button>
-            <div>
-              <button type="button">
+            <div className="flex flex-col gap-8">
+              <button
+                type="button"
+                className="flex items-center gap-2 p-2 border border-stroke rounded-full"
+              >
                 <Image
                   src="/test/photo-cat-test.png"
                   width={48}
@@ -32,8 +51,13 @@ export default function UploadView() {
                   alt="角龍寶寶"
                   className="rounded-full"
                 />
+                <div className="text-left">
+                  <p className="font-bold">角龍寶寶</p>
+                  <p className="text-note">@littleprincess126</p>
+                </div>
+                <IconChevronDown size={24} className="mr-3" />
               </button>
-              <button className="py-3 rounded-full bg-primary text-white font-bold">
+              <button className="py-3 w-full  rounded-full bg-primary text-white text-xl font-bold">
                 Pawk!
               </button>
             </div>
