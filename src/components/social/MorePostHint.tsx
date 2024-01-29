b081046931ec43bef81c8b82469081ead12bdc44
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-01-29 12:13:06 +0800

    style: add MorePostHint.tsx to post list

---
+++ src/components/social/MorePostHint.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+
+const MorePostHint: FC = () => {
+  return (
+    <section className="flex flex-col items-center gap-4 my-16 text-center">
+      <Image
+        src="/icons/icon-paw-gradient.svg"
+        width={162}
+        height={162}
+        alt="no content"
+      />
+      <p>
+        <p className="text-2xl">沒有其他新動態了</p>
+        <p className="text-note">你已看完過去3天所有追蹤中的貼文</p>
+      </p>
+      <p>
+        瞧瞧我們的{" "}
+        <Link href="/test/explore" className="text-primary font-bold">
+          探索
+        </Link>{" "}
+        或是繼續往下瀏覽{" "}
+        <Link href="#" className="text-primary font-bold">
+          熱門貼文
+        </Link>
+      </p>
+    </section>
+  );
+};
+
+export default MorePostHint;
