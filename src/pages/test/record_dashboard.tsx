a2faeb4fa7802f42f71046e2e10757aaa26dbc52
Finn Mao <loverainys@gmail.com>
2024-01-29 17:40:57 +0800

    refactor: refactor DataSummaryBtn component in RecodDashboard

--- src/pages/test/record_dashboard.tsx
+++ src/pages/test/record_dashboard.tsx
@@ -5,6 +5,16 @@ import RecordCard from "@/components/recordDashboard/RecordCard";
 import Footer from "@/components/petProfile/Footer";
 
 export default function RecordDashboard() {
+  const DataSummaryBtn = () => {
+    return (
+      <button
+        className="text-primary font-bold border border-stroke rounded-[30px] px-8 py-4 hover:cursor-pointer"
+        type="button"
+      >
+        查看數據總覽
+      </button>
+    );
+  };
   return (
     <section className="inner mt-[96px] flex flex-col gap-y-[64px]">
       <div className="flex justify-between gap-x-8">
@@ -15,12 +25,7 @@ export default function RecordDashboard() {
         <div className="flex flex-col gap-y-8 max-w-[480px]">
           <div className="flex justify-between">
             <PetAccount />
-            <button
-              className="text-primary font-bold border border-stroke rounded-[30px] px-8 py-4 hover:cursor-pointer"
-              type="button"
-            >
-              查看數據總覽
-            </button>
+            <DataSummaryBtn />
           </div>
           <RecordCard />
         </div>
