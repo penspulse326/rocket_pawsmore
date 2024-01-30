473b84b50fcbc4f8a565d61125b0fa4dfadc39aa
SHIN <akiyachiumi@gmail.com>
2024-01-31 07:15:17 +0800

    chore: add signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -78,8 +78,8 @@ const LoginPage: NextPage = () => {
           <section className="col-span-5 col-start-8 flex flex-col justify-center pr-12">
             <section className="flex flex-col justify-center gap-8 p-8 border border-note rounded-[30px]">
               <div>
-                <h2 className="text-[32px]">歡迎回來！</h2>
-                <h3 className="text-note">讓我們繼續記錄美好時光！</h3>
+                <h2 className="text-[32px]">註冊</h2>
+                <h3 className="text-note">一同開啟與毛孩相伴的精彩冒險！</h3>
               </div>
               <form onSubmit={handleLogin} className="flex flex-col gap-4">
                 <div className="flex flex-col gap-1">
@@ -92,28 +92,36 @@ const LoginPage: NextPage = () => {
                   />
                 </div>
                 <div className="flex flex-col gap-1">
+                  <div className="flex justify-between items-center">
+                    確認密碼
+                  </div>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="輸入8字符以上英數字密碼"
+                    className="p-3 w-full border border-stroke outline-note rounded-[10px] "
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
                   <div className="flex justify-between items-center">密碼</div>
                   <input
                     type="password"
                     name="password"
-                    placeholder="密碼"
+                    placeholder="再次輸入密碼"
                     className="p-3 w-full border border-stroke outline-note rounded-[10px] "
                   />
-                  <Link href="#" className="self-end text-primary">
-                    忘記密碼？
-                  </Link>
                 </div>
                 <button
                   type="submit"
                   className="mt-4 py-3 rounded-full bg-primary text-white"
                 >
-                  登入
+                  註冊
                 </button>
               </form>
               <div className="flex justify-center gap-4">
-                還沒有帳號？
-                <Link href="/signup" className="text-primary underline">
-                  立刻加入
+                已有帳號？
+                <Link href="/login" className="text-primary underline">
+                  加入
                 </Link>
               </div>
             </section>
