1d281c07ec7507384239ec97a86d9798240bbed8
Finn Mao <loverainys@gmail.com>
2024-01-30 18:25:25 +0800

    refactor: seperate components in Calendar layout

--- src/containers/recordDashboard/Calendar/HeaderLayout.tsx
+++ src/containers/recordDashboard/Calendar/HeaderLayout.tsx
@@ -1,142 +1,19 @@
 import moment from "moment";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   IconChevronLeft,
   IconChevronRight,
   IconChevronDown,
 } from "@tabler/icons-react";
 
-interface DataType {
-  card: string;
-  created_at: string;
-  category?: string;
-  content?: string | null;
-  type?: string;
-  remind_at?: string;
-  reserve_type?: string | null;
-  reserve_at?: string | null;
-  title?: string;
-}
-const originalDataset: DataType[] = [
-  {
-    card: "日常紀錄",
-    created_at: "2023-12-31",
-  },
-  {
-    card: "日常紀錄",
-    created_at: "2024-01-03",
-  },
-  {
-    card: "日常紀錄",
-    created_at: "2024-01-08",
-  },
-  {
-    card: "日常紀錄",
-    created_at: "2024-01-17",
-  },
-  {
-    card: "日常紀錄",
-    created_at: "2024-01-30",
-  },
-  {
-    card: "重要時刻",
-    created_at: "2024/01/17",
-    category: "驚喜",
-    content: "撿到乳牙",
-  },
-  {
-    card: "重要時刻",
-    created_at: "2024/02/02",
-    category: "驚喜",
-    content: "生日",
-  },
-  {
-    card: "重要時刻",
-    created_at: "2024/01/23",
-    category: "行為表現",
-    content: "睡到翻肚",
-  },
-  {
-    card: "重要時刻",
-    created_at: "2024/01/28",
-    category: "社交",
-    content: "交到新朋友",
-  },
-  {
-    card: "重要時刻",
-    created_at: "2024/01/20",
-    category: "社交",
-    content: "買新衣",
-  },
-  {
-    card: "醫療紀錄",
-    created_at: "2024-01-20",
-    type: "醫療提醒",
-    remind_at: "2024/01/25",
-    reserve_type: "健檢",
-    reserve_at: "2024-01-25",
-  },
-  {
-    card: "醫療紀錄",
-    created_at: "2024-01-20",
-    type: "醫療提醒",
-    remind_at: "2024-02-01",
-    reserve_type: "看診",
-    reserve_at: "2024-02-01",
-  },
-  {
-    card: "醫療紀錄",
-    created_at: "2024-01-20",
-    type: "醫療提醒",
-    remind_at: "2024-02-10",
-    reserve_type: "打疫苗",
-    reserve_at: "2024-02-10",
-  },
-  {
-    card: "醫療紀錄",
-    created_at: "2024-01-10",
-    type: "醫療提醒",
-    remind_at: "2024-01-17",
-    reserve_type: "驅蟲",
-    reserve_at: "2024-01-17",
-  },
-  {
-    card: "醫療紀錄",
-    created_at: "2024-01-10",
-    type: "醫療提醒",
-    remind_at: "2024-01-17",
-    reserve_type: "洗牙",
-    reserve_at: "2024-01-17",
-  },
-  {
-    card: "醫療紀錄",
-    created_at: "2024-01-09",
-    type: "就診紀錄",
-    title: "六合一疫苗初施",
-    remind_at: "2024-01-13",
-    reserve_type: null,
-    reserve_at: null,
-  },
-  {
-    card: "醫療紀錄",
-    created_at: "2024-01-16",
-    type: "就診紀錄",
-    title: "回診",
-    remind_at: "2024-01-13",
-    reserve_type: null,
-    reserve_at: null,
-  },
-];
-
-let eventData = originalDataset;
-
 const HeaderLayout = () => {
   const [filterEvent, setFilterEvent] = useState("全部類型");
-
   const [currentDate, setCurrentDate] = useState(moment());
   const [isExpanded, setIsExpanded] = useState(false);
 
   const category: string[] = ["全部類型", "日常紀錄", "醫療紀錄", "重要時刻"];
+
+  useEffect(() => {}, [filterEvent]);
 
   const CalendarTitle = () => {
     return (
@@ -175,20 +52,19 @@ const HeaderLayout = () => {
     );
   };
   const EventFilter = () => {
-    const handleEventFilter = (prop: string) => {
-      switch (prop) {
-        case "全部類型":
-          eventData = originalDataset;
-          setFilterEvent(prop);
-          break;
-        default:
-          eventData = originalDataset.filter((event) => event.card === prop);
-          setFilterEvent(prop);
-          break;
-      }
-    };
+    // const handleEventFilter = (prop: string) => {
+    //   switch (prop) {
+    //     case "全部類型":
+    //       // eventData = originalData;
+    //       setFilterEvent(prop);
+    //       break;
+    //     default:
+    //       // eventData = originalData.filter((event) => event.card === prop);
+    //       setFilterEvent(prop);
+    //       break;
+    //   }
+    // };
 
-    // helper
     const iconFillColor = (prop: string) => {
       switch (prop) {
         case "日常紀錄":
@@ -217,7 +93,7 @@ const HeaderLayout = () => {
                   className="flex items-center gap-x-1 px-3 py-1 rounded-3xl hover:cursor-pointer hover:bg-secondary"
                   key={index}
                   onClick={() => {
-                    handleEventFilter(item);
+                    setFilterEvent(item);
                     setIsExpanded(!isExpanded);
                   }}
                 >
