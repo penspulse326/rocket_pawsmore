a559559609a5985c9628bdb322a061b98d37ef93
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-01-30 11:40:14 +0800

    refactor: redesign mask and add esc trigger

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,3 +1,12 @@
+interface PostDataType {
+  created_at: string;
+  content: string;
+  media_type: string;
+  media_url: string;
+  likes: number;
+  extra: {};
+}
+
 interface CommentDataType {
   photoUrl: string;
   username: string;
@@ -5,4 +14,4 @@ interface CommentDataType {
   time: string;
 }
 
-export type { CommentDataType };
+export type { CommentDataType, PostDataType };
