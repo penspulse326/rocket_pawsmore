6001f57a2d8ce0fab5e22c9eace10cb984cdcd02
SHIN <akiyachiumi@gmail.com>
2024-01-31 04:45:36 +0800

    style: re-disign layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 
 import { useDispatch } from "react-redux";
 import { setUserInfo } from "@/common/redux/userInfoSlice";
+import Footer from "@/components/petProfile/Footer";
 
 const LoginPage: NextPage = () => {
   const dispatch = useDispatch();
@@ -38,53 +39,82 @@ const LoginPage: NextPage = () => {
   };
 
   return (
-    <section className="outer flex justify-center items-center gap-20 h-screen">
-      <section className="flex items-center gap-8">
-        <Image
-          src="/images/logo-rounded.svg"
-          alt="logo"
-          width={144}
-          height={144}
-        />
-        <Image
-          src="/images/logo-text.svg"
-          alt="logo-text"
-          width={0}
-          height={0}
-          className="w-auto h-auto"
-        />
-      </section>
-      <section className="flex flex-col justify-center items-center gap-8 p-8 w-[30%] border border-note rounded-[30px] shadow-lg">
-        <h2 className="text-3xl font-bold">登入</h2>
-        <form className="flex flex-col gap-4 w-full" onSubmit={handleLogin}>
-          <input
-            type="text"
-            name="email"
-            placeholder="電子信箱"
-            className="p-3 w-full border border-stroke rounded-md outline-note"
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="密碼"
-            className="p-3 w-full border border-stroke rounded-md"
-          />
-          <Link href="#" className="self-end underline">
-            忘記密碼
-          </Link>
-          <button
-            type="submit"
-            className="py-3 rounded-full bg-primary text-white"
-          >
-            登入
-          </button>
-        </form>
-        <div className="flex gap-8">
-          <Link href="#" className="underline">
-            還沒有帳號？點此註冊
-          </Link>
-        </div>
-      </section>
+    <section className="outer">
+      <div className="inner flex flex-col h-screen">
+        <section className="flex-grow grid grid-cols-12 gap-8">
+          {/* logo 圖片 */}
+          <section className="col-span-6 flex flex-col justify-center gap-12 pl-12">
+            <div className="flex items-center gap-4">
+              <Image
+                src="/images/logo-rounded.svg"
+                alt="logo"
+                width={82}
+                height={82}
+              />
+              <Image
+                src="/images/logo-text.svg"
+                alt="logo-text"
+                width={297}
+                height={62}
+              />
+            </div>
+            <Image
+              src="/images/banner-home.png"
+              alt="home-banner"
+              width={563}
+              height={70}
+            />
+          </section>
+          {/* 表單 */}
+          <section className="col-span-5 col-start-8 flex flex-col justify-center pr-12">
+            <section className="flex flex-col justify-center gap-8 p-8 border border-note rounded-[30px]">
+              <div>
+                <h2 className="text-[32px]">歡迎回來！</h2>
+                <h3 className="text-note">讓我們繼續記錄美好時光！</h3>
+              </div>
+              <form onSubmit={handleLogin} className="flex flex-col gap-4">
+                <div className="flex flex-col gap-1">
+                  <div className="flex justify-between items-center">Email</div>
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="輸入電子郵件地址"
+                    className="p-3 w-full border border-stroke outline-note rounded-[10px] "
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <div className="flex justify-between items-center">密碼</div>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="密碼"
+                    className="p-3 w-full border border-stroke outline-note rounded-[10px] "
+                  />
+                  <Link href="#" className="self-end text-primary">
+                    忘記密碼？
+                  </Link>
+                </div>
+                <button
+                  type="submit"
+                  className="mt-4 py-3 rounded-full bg-primary text-white"
+                >
+                  登入
+                </button>
+              </form>
+              <div className="flex justify-center gap-4">
+                還沒有帳號？
+                <Link href="#" className="text-primary underline">
+                  立刻加入
+                </Link>
+              </div>
+            </section>
+          </section>
+          {/* footer */}
+        </section>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </section>
   );
 };
