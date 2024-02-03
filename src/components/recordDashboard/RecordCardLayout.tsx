72d649ef4b61acb094a4451c05df337788482dd2
Finn Mao <loverainys@gmail.com>
2024-02-03 20:17:15 +0800

    style: add scrollbar and set container max height

--- src/components/recordDashboard/RecordCardLayout.tsx
+++ src/components/recordDashboard/RecordCardLayout.tsx
@@ -7,7 +7,7 @@ const RecordCardLayout: React.FC = () => {
   return (
     <section className="flex flex-col gap-y-8 border border-stroke rounded-[30px] p-8 max-w-[480px]">
       <Date />
-      <div className="flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-4 max-h-[826px] overflow-y-auto overflow-x-hidden">
         <Cards />
         <AddRecordBtn />
       </div>
