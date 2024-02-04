601eed8121104f15dedbd00219e881b5ebf5ac60
Finn Mao <loverainys@gmail.com>
2024-02-04 17:34:02 +0800

    refactor: adjust  SingleCardLayout structure

--- src/containers/recordCard/SingleCardLayout.tsx
+++ src/containers/recordCard/SingleCardLayout.tsx
@@ -22,19 +22,16 @@ const SingleCardLayout: React.FC<SingleCardPropsType> = ({
   const Content: React.FC = () => {
     const Reminder: React.FC = () => {
       return (
-        <>
-          <ul className="flex flex-col gap-y-2">
-            <ol className="flex gap-x-8">
-              <li className="font-semibold">預約類型</li>
-              <li>{data.reserve_type}</li>
-            </ol>
-            <ol className="flex gap-x-8">
-              <li className="font-semibold">預約時間</li>
-              <li>{moment(data.reserve_at).format("YYYY/M/D HH:MM")}</li>
-            </ol>
-          </ul>
-          <ShareBtn />
-        </>
+        <ul className="flex flex-col gap-y-2">
+          <ol className="flex gap-x-8">
+            <li className="font-semibold">預約類型</li>
+            <li>{data.reserve_type}</li>
+          </ol>
+          <ol className="flex gap-x-8">
+            <li className="font-semibold">預約時間</li>
+            <li>{moment(data.reserve_at).format("YYYY/M/D HH:MM")}</li>
+          </ol>
+        </ul>
       );
     };
     const Moment: React.FC = () => {
@@ -55,43 +52,40 @@ const SingleCardLayout: React.FC<SingleCardPropsType> = ({
         }
       };
       return (
-        <>
-          <ul className="flex flex-col gap-y-3">
-            <ol className="flex gap-x-12">
-              <li className="font-semibold min-w-[64px]">事件分類</li>
-              <li>
-                {data.category && (
-                  <span
-                    className={`px-2 rounded-[30px] ${tagBackgroundColor(
-                      data.category
-                    )}`}
-                  >
-                    {data.category}
-                  </span>
-                )}
-              </li>
-            </ol>
-            <ol className="flex gap-x-12">
-              <li className="font-semibold min-w-[64px]">內容</li>
-              <li>{data.content}</li>
-            </ol>
-            <ol className="flex gap-x-12">
-              <li className="font-semibold min-w-[64px]">紀錄照片</li>
-              <Image
-                className="rounded-[10px]"
-                src={data.photo!}
-                width={248}
-                height={168}
-                alt="moment photo"
-              />
-            </ol>
-            <ol className="flex gap-x-12">
-              <li className="font-semibold min-w-[64px]">事件描述</li>
-              <li>{data.desc}</li>
-            </ol>
-          </ul>
-          <ShareBtn />
-        </>
+        <ul className="flex flex-col gap-y-3">
+          <ol className="flex gap-x-12">
+            <li className="font-semibold min-w-[64px]">事件分類</li>
+            <li>
+              {data.category && (
+                <span
+                  className={`px-2 rounded-[30px] ${tagBackgroundColor(
+                    data.category
+                  )}`}
+                >
+                  {data.category}
+                </span>
+              )}
+            </li>
+          </ol>
+          <ol className="flex gap-x-12">
+            <li className="font-semibold min-w-[64px]">內容</li>
+            <li>{data.content}</li>
+          </ol>
+          <ol className="flex gap-x-12">
+            <li className="font-semibold min-w-[64px]">紀錄照片</li>
+            <Image
+              className="rounded-[10px]"
+              src={data.photo!}
+              width={248}
+              height={168}
+              alt="moment photo"
+            />
+          </ol>
+          <ol className="flex gap-x-12">
+            <li className="font-semibold min-w-[64px]">事件描述</li>
+            <li>{data.desc}</li>
+          </ol>
+        </ul>
       );
     };
     const Daily: React.FC = () => {
@@ -137,100 +131,90 @@ const SingleCardLayout: React.FC<SingleCardPropsType> = ({
       ];
 
       return (
-        <>
-          <div className="flex flex-col gap-y-4">
-            {/* routine record */}
-            {hasRoutineRecord && (
-              <ToggleList title={"一般"}>
-                <div className="flex flex-col gap-y-2">
-                  {routineRecord
-                    .filter((item) => item.content)
-                    .map((item, index) => {
-                      return (
-                        <ul key={index} className="flex gap-x-4">
-                          <li className="font-semibold min-w-12">
-                            {item.title}
-                          </li>
-                          <ol className="flex gap-x-1">
-                            <li>{item.content} </li>
-                            <li>{item.title === "飲水量" ? "ml" : "kg"}</li>
-                          </ol>
-                        </ul>
-                      );
-                    })}
-                  {/* food intakes */}
-                  {data.food && (
-                    <div className="flex gap-x-4">
-                      <div className="font-semibold min-w-12">進食量</div>
-                      <div className="flex flex-col">
-                        {data.food.map((item, index) => {
-                          return (
-                            <ul key={index} className="flex gap-x-1">
-                              <li>{item.food_type}</li>
-                              <li>{item.food_weight}</li>
-                              <li>g</li>
-                            </ul>
-                          );
-                        })}
-                      </div>
+        <div className="flex flex-col gap-y-4">
+          {/* routine record */}
+          {hasRoutineRecord && (
+            <ToggleList title={"一般"}>
+              <div className="flex flex-col gap-y-2">
+                {routineRecord
+                  .filter((item) => item.content)
+                  .map((item, index) => {
+                    return (
+                      <ul key={index} className="flex gap-x-4">
+                        <li className="font-semibold min-w-12">{item.title}</li>
+                        <ol className="flex gap-x-1">
+                          <li>{item.content} </li>
+                          <li>{item.title === "飲水量" ? "ml" : "kg"}</li>
+                        </ol>
+                      </ul>
+                    );
+                  })}
+                {/* food intakes */}
+                {data.food && (
+                  <div className="flex gap-x-4">
+                    <div className="font-semibold min-w-12">進食量</div>
+                    <div className="flex flex-col">
+                      {data.food.map((item, index) => {
+                        return (
+                          <ul key={index} className="flex gap-x-1">
+                            <li>{item.food_type}</li>
+                            <li>{item.food_weight}</li>
+                            <li>g</li>
+                          </ul>
+                        );
+                      })}
                     </div>
-                  )}
-                </div>
-              </ToggleList>
-            )}
-            {/* the abnormals */}
-            {isAbnormal && (
-              <ToggleList title={"異常"}>
-                <div className="flex flex-col gap-y-2">
-                  {abnormal
-                    .filter((item) => item.content)
-                    .map((item, index) => {
-                      return (
-                        <ul key={index} className="flex gap-x-4">
-                          <li className="font-semibold min-w-12">
-                            {item.title}
-                          </li>
-                          <li>
-                            {item.title === "症狀" &&
-                            Array.isArray(item.content)
-                              ? item.content.join("、")
-                              : item.content}
-                          </li>
-                        </ul>
-                      );
-                    })}
-                </div>
-              </ToggleList>
-            )}
-            {/* daily cares */}
-            {hasDailyCares && (
-              <ToggleList title={"日常照護"}>
-                <div className="flex flex-col gap-y-2">
-                  {dailyCares
-                    .filter((item) => item.content)
-                    .map((item, index) => {
-                      return (
-                        <ul key={index} className="flex gap-x-4">
-                          <li className="font-semibold min-w-12">
-                            {item.title}
-                          </li>
-                          <li>{item.content}</li>
-                        </ul>
-                      );
-                    })}
-                </div>
-              </ToggleList>
-            )}
-            {/* note */}
-            {data.remark && (
-              <ul className="flex flex-col gap-y-2">
-                <li className="text-note">備註</li>
-                <li>{data.remark}</li>
-              </ul>
-            )}
-          </div>
-          <ShareBtn />
-        </>
+                  </div>
+                )}
+              </div>
+            </ToggleList>
+          )}
+          {/* the abnormals */}
+          {isAbnormal && (
+            <ToggleList title={"異常"}>
+              <div className="flex flex-col gap-y-2">
+                {abnormal
+                  .filter((item) => item.content)
+                  .map((item, index) => {
+                    return (
+                      <ul key={index} className="flex gap-x-4">
+                        <li className="font-semibold min-w-12">{item.title}</li>
+                        <li>
+                          {item.title === "症狀" && Array.isArray(item.content)
+                            ? item.content.join("、")
+                            : item.content}
+                        </li>
+                      </ul>
+                    );
+                  })}
+              </div>
+            </ToggleList>
+          )}
+          {/* daily cares */}
+          {hasDailyCares && (
+            <ToggleList title={"日常照護"}>
+              <div className="flex flex-col gap-y-2">
+                {dailyCares
+                  .filter((item) => item.content)
+                  .map((item, index) => {
+                    return (
+                      <ul key={index} className="flex gap-x-4">
+                        <li className="font-semibold min-w-12">{item.title}</li>
+                        <li>{item.content}</li>
+                      </ul>
+                    );
+                  })}
+              </div>
+            </ToggleList>
+          )}
+          {/* note */}
+          {data.remark && (
+            <ul className="flex flex-col gap-y-2">
+              <li className="text-note">備註</li>
+              <li>{data.remark}</li>
+            </ul>
+          )}
+        </div>
       );
     };
     const Medical: React.FC = () => {
@@ -251,52 +235,47 @@ const SingleCardLayout: React.FC<SingleCardPropsType> = ({
         { title: "居家注意事項", content: data.notice },
       ];
       return (
-        <>
-          <div className="flex flex-col gap-y-4">
-            <ul className="flex flex-col gap-y-2">
-              {medicalData
-                .filter((item) => item.content)
-                .map((item, index) => {
-                  return (
-                    <ol key={index} className="flex gap-x-6">
-                      <li className="font-semibold min-w-[96px]">
-                        {item.title}
-                      </li>
-                      <li>{item.content}</li>
-                    </ol>
-                  );
-                })}
-              {data.cost && (
-                <ol className="flex gap-x-6">
-                  <li className="font-semibold min-w-[96px]">開銷</li>
-                  <ul className="flex gap-x-1">
-                    <li>NTD</li>
-                    <li>{costFormat(data.cost)}</li>
-                  </ul>
-                </ol>
-              )}
-              {data.photo && (
-                <ol className="flex gap-x-6">
-                  <li className="font-semibold min-w-[96px]">紀錄照片</li>
-                  <Image
-                    className="rounded-[10px]"
-                    src={data.photo}
-                    width={248}
-                    height={186}
-                    alt="photo"
-                  />
-                </ol>
-              )}
-              {data.remind_at && (
-                <ol className="flex gap-x-6">
-                  <li className="font-semibold min-w-[96px]">回診提醒</li>
-                  <li>{moment(data.remind_at).format("YYYY/M/D")}</li>
-                </ol>
-              )}
-            </ul>
-          </div>
-          <ShareBtn />
-        </>
+        <div className="flex flex-col gap-y-4">
+          <ul className="flex flex-col gap-y-2">
+            {medicalData
+              .filter((item) => item.content)
+              .map((item, index) => {
+                return (
+                  <ol key={index} className="flex gap-x-6">
+                    <li className="font-semibold min-w-[96px]">{item.title}</li>
+                    <li>{item.content}</li>
+                  </ol>
+                );
+              })}
+            {data.cost && (
+              <ol className="flex gap-x-6">
+                <li className="font-semibold min-w-[96px]">開銷</li>
+                <ul className="flex gap-x-1">
+                  <li>NTD</li>
+                  <li>{costFormat(data.cost)}</li>
+                </ul>
+              </ol>
+            )}
+            {data.photo && (
+              <ol className="flex gap-x-6">
+                <li className="font-semibold min-w-[96px]">紀錄照片</li>
+                <Image
+                  className="rounded-[10px]"
+                  src={data.photo}
+                  width={248}
+                  height={186}
+                  alt="photo"
+                />
+              </ol>
+            )}
+            {data.remind_at && (
+              <ol className="flex gap-x-6">
+                <li className="font-semibold min-w-[96px]">回診提醒</li>
+                <li>{moment(data.remind_at).format("YYYY/M/D")}</li>
+              </ol>
+            )}
+          </ul>
+        </div>
       );
     };
 
@@ -345,7 +324,12 @@ const SingleCardLayout: React.FC<SingleCardPropsType> = ({
         )}
       </div>
       {/* content */}
-      {isOpened && <Content />}
+      {isOpened && (
+        <>
+          <Content />
+          <ShareBtn />
+        </>
+      )}
     </div>
   );
 };
