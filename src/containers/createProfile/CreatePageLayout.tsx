7b97213f6364fd8a2cb555aabba6c2b6952b66de
SHIN <akiyachiumi@gmail.com>
2024-02-06 07:43:51 +0800

    chore: add CreatePhase.tsx

--- src/containers/createProfile/CreatePageLayout.tsx
+++ src/containers/createProfile/CreatePageLayout.tsx
@@ -1,9 +1,13 @@
+import CreatePhase from "./CreatePhase";
+
 const CreatePageLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
     <section className="outer px-8">
-      <div className="inner pt-16"></div>
+      <div className="inner pt-16">
+        <CreatePhase />
+      </div>
     </section>
   );
 };
