77e4ff5baf97846bc710cc046ed7f7d6d7637c97
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2024-02-06 13:52:17 +0800

    fixed: input style change when error happened

---
+++ src/components/form/Input.tsx
@@ -0,0 +1,39 @@
+import React, { forwardRef } from "react";
+import ErrorMessage from "../ErrorMessage";
+
+// apply react-hook-form
+interface InputPropsType {
+  name: string;
+  title: string;
+  placeholder: string;
+  message?: string;
+  onChange?: (...event: any[]) => void;
+  onBlur?: () => void;
+}
+
+const Input = forwardRef<HTMLInputElement, InputPropsType>(
+  ({ name, title, placeholder, message, onChange }, ref) => {
+    const borderStyle = message ? { border: "1px solid #F23030" } : {};
+
+    return (
+      <div className="flex flex-col gap-1">
+        <h4 className="flex justify-between items-center">
+          <span>{title}</span>
+          {message && <ErrorMessage>{message}</ErrorMessage>}
+        </h4>
+        <input
+          type="text"
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          ref={ref}
+          style={borderStyle}
+          className="p-3 w-full outline-0 border border-stroke rounded-[10px] focus:border-note"
+        />
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
+export default Input;
--- src/containers/home/SignUp.tsx
+++ src/containers/home/SignUp.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 import ErrorMessage from "@/components/ErrorMessage";
 import { errorText } from "@/common/lib/messageText";
+import Input from "@/components/form/Input";
 
 interface SignUpPropsType {
   onSubmit: (data: any) => void;
@@ -18,6 +19,7 @@ const SignUp: React.FC<SignUpPropsType> = ({ onSubmit: handleSignUp }) => {
   const {
     register,
     handleSubmit,
+    control,
     watch,
     formState: { errors },
   } = useForm<FormInputType>();
@@ -34,63 +36,61 @@ const SignUp: React.FC<SignUpPropsType> = ({ onSubmit: handleSignUp }) => {
           <h3 className="text-note">一同開啟與毛孩相伴的精彩冒險！</h3>
         </div>
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-          <div className="flex flex-col gap-1">
-            <h4 className="flex justify-between items-center">
-              <span>Email</span>
-              {errors.email && (
-                <ErrorMessage>{errors.email?.message}</ErrorMessage>
-              )}
-            </h4>
-            <input
-              {...register("email", {
-                required: errorText.REQUIRED,
-                pattern: {
-                  value: /\S+@\S+\.\S+/,
-                  message: errorText.EMAIL_INVALID,
-                },
-              })}
-              type="text"
-              name="email"
-              placeholder="輸入電子郵件地址"
-              className="p-3 w-full border border-stroke outline-note rounded-[10px] "
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <h4 className="flex justify-between items-center">
-              <span>密碼</span>
-              <ErrorMessage>{errors.password?.message}</ErrorMessage>
-            </h4>
-            <input
-              {...register("password", {
-                required: errorText.REQUIRED,
-                pattern: {
-                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-                  message: errorText.PASSWORD_INVALID,
-                },
-              })}
-              type="password"
-              name="password"
-              placeholder="輸入8字符以上英數字密碼"
-              className="p-3 w-full border border-stroke outline-note rounded-[10px] "
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <h4 className="flex justify-between items-center">
-              <span>確認密碼</span>
-              <ErrorMessage>{errors.checkPassword?.message}</ErrorMessage>
-            </h4>
-            <input
-              {...register("checkPassword", {
-                required: errorText.REQUIRED,
-                validate: (value) =>
-                  value === password || errorText.PASSWORD_NOT_MATCH,
-              })}
-              type="password"
-              name="checkPassword"
-              placeholder="再次輸入密碼"
-              className="p-3 w-full border border-stroke outline-note rounded-[10px] "
-            />
-          </div>
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              required: errorText.REQUIRED,
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: errorText.EMAIL_INVALID,
+              },
+            }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                title="Email"
+                placeholder="輸入電子郵件地址"
+                message={errors.email?.message}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            rules={{
+              required: errorText.REQUIRED,
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                message: errorText.PASSWORD_INVALID,
+              },
+            }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                title="密碼"
+                placeholder="輸入8個字以上英數字"
+                message={errors.password?.message}
+              />
+            )}
+          />
+          <Controller
+            name="checkPassword"
+            control={control}
+            rules={{
+              required: errorText.REQUIRED,
+              validate: (value) =>
+                value === password || errorText.PASSWORD_NOT_MATCH,
+            }}
+            render={({ field }) => (
+              <Input
+                {...field}
+                title="確認密碼"
+                placeholder="再次輸入密碼"
+                message={errors.checkPassword?.message}
+              />
+            )}
+          />
           <button
             type="submit"
             className="mt-4 py-3 rounded-full bg-primary text-white"
