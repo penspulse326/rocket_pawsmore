3f85d36d1d774007997d16690fc7460464d91678
Finn Mao <loverainys@gmail.com>
2024-02-07 10:53:24 +0800

    fixed: remove introduction

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -39,7 +39,6 @@ export default async function handler(
         username: "琪琪",
         account: "chichi1992126",
         photoUrl: "/test/user-chichi.png",
-        introduction: "這是一個測試用的介紹",
       },
     });
   } catch (error) {
