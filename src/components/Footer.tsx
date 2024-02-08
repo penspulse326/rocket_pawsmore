421bfba75affd95053ba16c1a1ec556a87b6571b
Finn Mao <loverainys@gmail.com>
2024-02-08 18:22:06 +0800

    fixed: Footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="my-8 text-note">
+    <footer className="py-8 text-note">
       <ul className="flex flex-col gap-y-4">
         <ol className="flex justify-center gap-x-4">
           <li>常見問答</li>
