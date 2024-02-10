ea4ec23111d0bc5017a4bbf8d8bf7607b276bc3f
Finn Mao <loverainys@gmail.com>
2024-02-10 22:32:37 +0800

    feat: test Link route

--- src/pages/user_dashboard/index.tsx
+++ src/pages/user_dashboard/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
+import { useSearchParams } from "next/navigation";
 import SideBar from "@/containers/userDashboard/SideBar";
 import MainContent from "@/containers/userDashboard/MainContent";
 import Footer from "@/components/Footer";
 
 const UserDashboard: React.FC = () => {
+  const params = useSearchParams();
+  const to = params.get("to");
+
   const [targetPage, setTargetPage] = useState("");
 
   return (
     <main className="outer pt-16 flex h-screen relative">
-      <SideBar prop="user" setTargetPage={setTargetPage} />
+      <SideBar prop={to as string} setTargetPage={setTargetPage} />
       <div className="flex flex-col gap-y-16 justify-between w-full pl-[248px]">
         <MainContent targetPage={targetPage} />
         <Footer />
