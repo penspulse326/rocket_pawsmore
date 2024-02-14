4e6b025e1606b3c3eb0b2c819c104d03df420339
penspulse326 <penspulse@gmail.com>
2024-02-14 11:12:43 +0800

    chore: rename files

---
+++ src/containers/post/Pawk.tsx
@@ -0,0 +1,52 @@
+import { IconPhoto, IconMovie, IconMedal } from "@tabler/icons-react";
+import { useState } from "react";
+
+import Mask from "../../components/hint/Mask";
+import UploadView from "../../components/post/UploadView";
+
+const Pawk = () => {
+  const [isUploadOpen, setIsUploadOpen] = useState(false);
+
+  return (
+    <section>
+      {/* 貼文按鈕 */}
+      <div
+        className="px-8 pt-8 pb-6 border border-stroke rounded-[30px]"
+        onClick={() => setIsUploadOpen(!isUploadOpen)}
+      >
+        {isUploadOpen && (
+          <Mask maskType="upload" setIsOpen={setIsUploadOpen}>
+            <UploadView setIsOpen={setIsUploadOpen} />
+          </Mask>
+        )}
+        <div className="px-8 py-4 w-full border border-stroke rounded-[30px] outline-none text-note">
+          在想些什麼呢？
+        </div>
+        <div className="flex justify-between items-center mt-6">
+          <div className="flex gap-8 text-note">
+            <button type="button" className="flex items-center gap-2">
+              <IconPhoto className="stroke-black" />
+              附上照片
+            </button>
+            <button type="button" className="flex items-center gap-2">
+              <IconMovie className="stroke-black" />
+              附上影片
+            </button>
+            <button type="button" className="flex items-center gap-2">
+              <IconMedal className="stroke-black" />
+              附上里程碑
+            </button>
+          </div>
+          <button
+            type="button"
+            className="px-8 py-2 rounded-[30px] bg-primary text-white font-bold"
+          >
+            Pawk!
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Pawk;
