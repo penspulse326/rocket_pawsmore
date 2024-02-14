46c07198f571760a11d47a026211163c8f1c62ee
Finn Mao <loverainys@gmail.com>
2024-02-14 18:34:45 +0800

    chore: add check user profile api

--- src/pages/api/apiBase.ts
+++ src/pages/api/apiBase.ts
@@ -3,8 +3,10 @@ const apiUrl = process.env.API_URL;
 const apiBase = {
   SIGN_UP: `${apiUrl}/signup`,
   LOGIN: `${apiUrl}/login`,
+  CHECK_SELF: `${apiUrl}/member/profile`,
   CREATE_MEMBER: `${apiUrl}/member/profile/add`,
   CREATE_PET: `${apiUrl}/member/pets`,
+  GET_PET: `${apiUrl}/member/pets`, // 動態路由 /:petId
   UPDATE_PET: `${apiUrl}/member/pets/update`, // 動態路由 /:petId
   GET_PET_LIST: `${apiUrl}/member/pets/userpets`, // 動態路由 /:userId
   GET_POST_LIST: `${apiUrl}/social/posts`, // query ?limit=10&sort=created_at desc
