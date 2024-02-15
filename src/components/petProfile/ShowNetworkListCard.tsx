dbd25bbdadbdfa4441492533abd29978d872bbda
Finn Mao <loverainys@gmail.com>
2024-02-16 01:27:57 +0800

    feat: refactor component to use parameters for rendering by types

--- src/components/petProfile/ShowNetworkListCard.tsx
+++ src/components/petProfile/ShowNetworkListCard.tsx
@@ -1,63 +1,109 @@
-import { FC } from "react";
+import React from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import { IconX } from "@tabler/icons-react";
+import handleFreezeScroll from "@/common/helpers/handleFreezeScroll";
+
+interface UserListType {
+  userId: number;
+  userAccount: string;
+  userName: string;
+  userPhoto: string;
+}
 
 interface PropsType {
-  title: string;
+  type: "following" | "follower";
+  userList: UserListType[];
   isClosed: boolean;
   setIsClosed: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-interface ListType {
-  imgUrl: string;
-  userName: string;
-  userId: string;
+interface CardContentDataType {
+  type: string;
+  TITLE: string;
+  CONTENT: string;
 }
 
-const ShowNetworkListCard: FC<PropsType> = (props) => {
-  const { title, isClosed, setIsClosed } = props;
-  const networkList: ListType[] = [
-    { imgUrl: "/test/photo-user-1.jpg", userName: "梅子", userId: "@may99883" },
-    { imgUrl: "/test/photo-user-2.jpg", userName: "怡昕", userId: "@yisin___" },
-    { imgUrl: "/test/photo-user-1.jpg", userName: "梅子", userId: "@may99883" },
-    { imgUrl: "/test/photo-user-2.jpg", userName: "怡昕", userId: "@yisin___" },
-    { imgUrl: "/test/photo-user-1.jpg", userName: "梅子", userId: "@may99883" },
-    { imgUrl: "/test/photo-user-2.jpg", userName: "怡昕", userId: "@yisin___" },
-    { imgUrl: "/test/photo-user-1.jpg", userName: "梅子", userId: "@may99883" },
-    { imgUrl: "/test/photo-user-2.jpg", userName: "怡昕", userId: "@yisin___" },
+const NetworkList: React.FC<PropsType> = (props) => {
+  const { type, userList, isClosed, setIsClosed } = props;
+  const router = useRouter();
+
+  const cardContent: CardContentDataType[] = [
+    {
+      type: "follower",
+      TITLE: "粉絲",
+      CONTENT: "尚無粉絲",
+    },
+    {
+      type: "following",
+      TITLE: "追蹤中",
+      CONTENT: "尚無追蹤",
+    },
   ];
+
+  const selectedCard = cardContent.find((item) => item.type === type);
+
+  const handleCheckUser = (account: string) => {
+    router.push(`/member/${account}`);
+  };
+
+  const handleCloseCard = () => {
+    setIsClosed(!isClosed);
+    handleFreezeScroll(false);
+  };
+
   return (
-    <div className="flex flex-col gap-y-5 max-w-[322px] w-full max-h-[427px] h-full bg-white border border-stroke rounded-[32px] p-8">
+    <div className="flex flex-col gap-y-6 max-w-[322px] w-full max-h-[427px] h-full bg-white border border-stroke rounded-[32px] p-8">
       <div className="flex justify-between items-center">
-        <div className="text-2xl font-bold">{title}</div>
+        <div className="text-2xl font-bold">{selectedCard?.TITLE}</div>
         <IconX
           size={24}
           color={"#808080"}
           className="hover:cursor-pointer"
-          onClick={() => setIsClosed(!isClosed)}
+          onClick={handleCloseCard}
         />
       </div>
-      <ul className="flex flex-col gap-y-4 max-h-[304px] overflow-y-auto">
-        {networkList.map((item, index) => {
-          return (
-            <li className="flex gap-x-4" key={`${index}-${item.userName}`}>
-              <Image
-                className="rounded-[53.3px]"
-                src={item.imgUrl}
-                width={48}
-                height={48}
-                alt="close"
-              />
-              <ol>
-                <li>{item.userName}</li>
-                <li>{item.userId}</li>
-              </ol>
-            </li>
-          );
-        })}
+      <ul className="flex flex-col gap-y-4 max-h-[304px] h-full overflow-y-auto">
+        {userList.length > 0 ? (
+          userList.map((user, index) => {
+            const { userAccount, userName, userPhoto } = user;
+
+            return (
+              <li
+                className="flex gap-x-4 hover:cursor-pointer"
+                onClick={() => handleCheckUser(userAccount)}
+                key={index}
+              >
+                <div className="w-12 h-12">
+                  <Image
+                    className="w-full h-full rounded-[53.3px] object-cover"
+                    src={userPhoto}
+                    width={48}
+                    height={48}
+                    alt={userName}
+                  />
+                </div>
+                <ol>
+                  <li>{userName}</li>
+                  <li>@{userAccount}</li>
+                </ol>
+              </li>
+            );
+          })
+        ) : (
+          <div className="flex flex-col gap-y-4 items-center h-full">
+            <Image
+              src="/icons/icon-paw-gradient.svg"
+              width={162}
+              height={162}
+              alt="no followers"
+            />
+            <span className="text-lg">{selectedCard?.CONTENT}</span>
+          </div>
+        )}
       </ul>
     </div>
   );
 };
 
-export default ShowNetworkListCard;
+export default NetworkList;
