8ca5458455a5185413069dd1f2cee2bea16e0f46
Finn Mao <loverainys@gmail.com>
2024-02-15 16:51:19 +0800

    feat: fetch pet post data by api

--- src/components/petProfile/gallery/Posts.tsx
+++ src/components/petProfile/gallery/Posts.tsx
@@ -1,56 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+
 import Image from "next/image";
 import { IconHeartFilled, IconMessageCircle2Filled } from "@tabler/icons-react";
 
+import NoContent from "@/components/NoContent";
+
+import { RootState } from "@/common/redux/store";
+import { PostDataType } from "@/types";
+
 const Posts: React.FC = () => {
-  const allPics = [
-    { url: "/test/gallery-1.jpg", hasMilestone: false },
-    { url: "/test/gallery-2.jpg", hasMilestone: true },
-    { url: "/test/gallery-3.jpg", hasMilestone: false },
-    { url: "/test/gallery-4.jpg", hasMilestone: false },
-    { url: "/test/gallery-5.jpg", hasMilestone: false },
-    { url: "/test/gallery-6.jpg", hasMilestone: true },
-  ];
+  const router = useRouter();
+  const { id } = router.query;
+  const userInfo = useSelector((state: RootState) => state.userInfo);
+
+  const [post, setPost] = useState<PostDataType[] | undefined>();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(`/api/post/${id}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${userInfo.token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("failed");
+        }
+        const data = await response.json();
+        setPost(data.data);
+      } catch (error) {}
+    };
+    fetchPost();
+  }, [userInfo.token, id]);
+
   return (
-    <section className="flex gap-4 flex-wrap">
-      {allPics.map((value, index) => {
-        return (
-          <div
-            className="gallery-card relative max-w-[352px] w-full max-h-[352px] h-full z-0"
-            key={index}
-          >
-            <Image
-              className="rounded-[30px]"
-              src={value.url}
-              width={352}
-              height={352}
-              alt="gallery photos"
-            />
-            {/* milestone badge */}
-            {value.hasMilestone ? (
-              <Image
-                className="absolute bottom-5 left-5"
-                src="/test/milestone-1.svg"
-                width={74}
-                height={70}
-                alt="milestone badge"
-              />
-            ) : null}
-            {/* show favorite icon & comments */}
-            <ul className="overlay absolute top-0 -z-10 flex gap-x-4 justify-center items-center bg-black/50 w-full h-full text-white rounded-[30px]">
-              <li className="flex gap-x-1 items-center">
-                <IconHeartFilled size={26} />
-                <div>4</div>
-              </li>
-              <li className="flex gap-x-1 items-center">
-                <IconMessageCircle2Filled size={26} />
-                <div>1</div>
-              </li>
-            </ul>
-          </div>
-        );
-      })}
-    </section>
+    <>
+      {post ? (
+        <section className="flex gap-4 flex-wrap mr-auto">
+          {post.map((value, index) => {
+            return (
+              <div
+                className="gallery-card relative w-[352px] h-[352px] z-0"
+                key={index}
+              >
+                <Image
+                  className="w-full h-full rounded-[30px] object-cover"
+                  src={value.media}
+                  width={352}
+                  height={352}
+                  alt="gallery photos"
+                />
+                {/* milestone badge */}
+                {/* {value.hasMilestone ? (
+                <Image
+                  className="absolute bottom-5 left-5"
+                  src="/test/milestone-1.svg"
+                  width={74}
+                  height={70}
+                  alt="milestone badge"
+                />
+              ) : null} */}
+                {/* show favorite icon & comments */}
+                <ul className="overlay absolute top-0 -z-10 flex gap-x-4 justify-center items-center bg-black/50 w-full h-full text-white rounded-[30px]">
+                  <li className="flex gap-x-1 items-center">
+                    <IconHeartFilled size={26} />
+                    <div>4</div>
+                  </li>
+                  <li className="flex gap-x-1 items-center">
+                    <IconMessageCircle2Filled size={26} />
+                    <div>1</div>
+                  </li>
+                </ul>
+              </div>
+            );
+          })}
+        </section>
+      ) : (
+        <section className="mx-auto">
+          <NoContent />
+        </section>
+      )}
+    </>
   );
 };
 
