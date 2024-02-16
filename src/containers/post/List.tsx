e96a6630c7bebbda55a843f9fd15b88f3fed2a7e
penspulse326 <penspulse@gmail.com>
2024-02-16 10:22:11 +0800

    fixed: unlock like toggle api limit

--- src/containers/post/List.tsx
+++ src/containers/post/List.tsx
@@ -8,45 +8,47 @@ import MorePostHint from "./MorePostHint";
 import { fetchGetAllPosts } from "@/common/fetch/post";
 
 import type { PostDataType } from "@/types";
+import Loading from "@/components/hint/Loading";
 
 const List: React.FC = () => {
-  const { token } = useSelector((state: RootState) => state.userInfo);
   const [list, setList] = useState<PostDataType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     handleGetList();
-  }, [token]);
+  }, []);
 
   const handleGetList = async () => {
-    if (token) {
-      try {
-        const response = await fetchGetAllPosts(token);
-        const data: PostDataType[] = response.data;
-        const sortedList = data.sort(
-          (a, b) =>
-            new Date(b.createDate).getTime() - new Date(a.createDate).getTime()
-        );
-        setList(sortedList);
-        console.log(sortedList);
-      } catch (error) {
-        console.error(error);
-      }
+    try {
+      const response = await fetchGetAllPosts();
+      const data: PostDataType[] = response.data;
+      const sortedList = data.sort(
+        (a, b) =>
+          new Date(b.createDate).getTime() - new Date(a.createDate).getTime()
+      );
+      setList(sortedList);
+    } catch (error) {
+      console.error(error);
     }
+    setIsLoading(false);
   };
 
   return (
-    <div className="scrollbar-none col-span-6 p-8 border-x border-stroke bg-white overflow-y-scroll">
-      <BtnPawk />
-      <h2 className="mt-8 text-note">動態消息</h2>
-      {/* 貼文列表 */}
-      <div className="flex flex-col gap-8 mt-4">
-        {/* 貼文卡片 */}
-        {list?.map((data) => (
-          <Card key={data.postId} data={data} getList={handleGetList} />
-        ))}
+    <>
+      <div className="scrollbar-none col-span-6 p-8 border-x border-stroke bg-white overflow-y-scroll">
+        <BtnPawk />
+        <h2 className="mt-8 text-note">動態消息</h2>
+        {/* 貼文列表 */}
+        <div className="flex flex-col gap-8 mt-4">
+          {/* 貼文卡片 */}
+          {list?.map((data) => (
+            <Card key={data.postId} data={data} getList={handleGetList} />
+          ))}
+        </div>
+        {/* <MorePostHint /> */}
       </div>
-      <MorePostHint />
-    </div>
+      {isLoading && <Loading />}
+    </>
   );
 };
 
