bab906161ec753a3ecc6d4680c22cb2d5773750e
Finn Mao <loverainys@gmail.com>
2024-02-16 11:39:36 +0800

    feat: fetch pet post data and create post list context

--- src/pages/pet/[petAccount].tsx
+++ src/pages/pet/[petAccount].tsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { createContext, useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import ProfileCard from "@/components/petProfile/ProfileCard";
 import ProfileGallery from "@/components/petProfile/ProfileGallery";
 import Footer from "@/components/Footer";
 
+import { PostDataType } from "@/types";
+
+export const PostListContext = createContext<PostDataType[] | undefined>(
+  undefined
+);
+
 const PetProfile: React.FC = () => {
+  const [postList, setPostList] = useState<PostDataType[]>();
+
+  const router = useRouter();
+  const { petAccount } = router.query;
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(`/api/post/${petAccount}`, {
+          method: "GET",
+        });
+        if (!response.ok) {
+          throw new Error("failed");
+        }
+        const data = await response.json();
+        setPostList(data.data);
+      } catch (error) {}
+    };
+    petAccount && fetchPost();
+  }, [petAccount]);
+
   return (
-    <main className="flex flex-col gap-y-12 items-center mt-[64px]">
-      <ProfileCard />
-      <ProfileGallery />
-      <Footer />
-    </main>
+    <PostListContext.Provider value={postList}>
+      <main className="flex flex-col gap-y-12 items-center mt-[64px]">
+        <ProfileCard />
+        <ProfileGallery />
+        <Footer />
+      </main>
+    </PostListContext.Provider>
   );
 };
 
