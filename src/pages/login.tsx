babde87befb59abbe7ec5ac0e95806d60e4634fd
penspulse326 <penspulse@gmail.com>
2024-02-17 11:35:42 +0800

    fixed: image alert

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,33 +1,9 @@
-import { GetServerSideProps } from "next";
 import { ReactElement } from "react";
 
 import HomeLayout from "@/containers/auth/Layout";
 import Login from "@/containers/auth/Login";
-import { fetchCheckAuth } from "@/common/fetch/auth";
 
 import type { NextPageWithLayout } from "./_app";
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const token = context.req.cookies.token;
-
-  // 如果 token 存在並合法，直接重導至 /social
-  if (token) {
-    const response = await fetchCheckAuth(token);
-
-    if (response.ok) {
-      return {
-        redirect: {
-          destination: "/social",
-          permanent: false,
-        },
-      };
-    }
-  }
-
-  return {
-    props: {},
-  };
-};
 
 const LoginPage: NextPageWithLayout = () => {
   return <Login />;
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,17 +1,8 @@
-import { useRouter } from "next/router";
-import { useState } from "react";
-
-import { useDispatch } from "react-redux";
-import { setUserInfo } from "@/common/redux/userInfoSlice";
-
 import HomeLayout from "@/containers/auth/Layout";
 import SignUp from "@/containers/auth/SignUp";
-import Loading from "@/components/hint/Loading";
-import apiNext from "../common/fetch/apiNext";
 
 import type { ReactElement } from "react";
 import type { NextPageWithLayout } from "../pages/_app";
-import type { LoginFormType } from "@/types";
 
 const SignUpPage: NextPageWithLayout = () => {
   return <SignUp />;
