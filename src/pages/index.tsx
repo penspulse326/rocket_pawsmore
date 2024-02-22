4b7931292f627936763b077bb6073e54e094dda7
SHIN <akiyachiumi@gmail.com>
2024-02-22 21:31:34 +0800

    chore: install cookie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
+import cookie from "cookie";
 import Layout from "@/containers/social/Layout";
 import Posts from "@/containers/social/posts";
+import { GetServerSideProps } from "next";
 import { fetchGetAllPosts } from "@/common/fetch/post";
 
 import type { ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 import type { PostDataType } from "@/types";
-import { GetServerSideProps } from "next";
 
 const SocialPage: NextPageWithLayout<{ data: PostDataType[] }> = ({ data }) => {
   return <Posts initialList={data} />;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookie = context.req.headers.cookie;
+
   try {
     const response = await fetchGetAllPosts();
     const data: PostDataType[] = response.data;
