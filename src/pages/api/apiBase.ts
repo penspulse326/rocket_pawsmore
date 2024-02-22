b783a55dc5418334a18255c81ccdf58a36cdaa03
Finn Mao <loverainys@gmail.com>
2024-02-22 23:57:59 +0800

    feat: add get pet record api

--- src/pages/api/apiBase.ts
+++ src/pages/api/apiBase.ts
@@ -8,16 +8,18 @@ const apiBase = {
   // 會員資料
   CHECK_MEMBER: `${apiUrl}/social/user`, // 動態路由 /:userAccount
   CREATE_MEMBER: `${apiUrl}/member/profile/add`,
+  // 寵物資料
   CREATE_PET: `${apiUrl}/member/pets`,
   GET_PET: `${apiUrl}/member/pets`, // 動態路由 /:petAccount
   UPDATE_PET: `${apiUrl}/member/pets/update`, // 動態路由 /:petId
   FOLLOW_PET: `${apiUrl}/social/follow`, // 動態路由 /:petAccount
   GET_PET_LIST: `${apiUrl}/member/pets/userpets`, // 動態路由 /:userId
+  GET_PET_POST: `${apiUrl}/social/posts/pet`, // 動態路由 /:petAccount
+  GET_PET_RECORD: `${apiUrl}/cards`, // 動態路由 /:petAccount
   // 貼文
   GET_ALL_POSTS: `${apiUrl}/social/posts/all`,
   GET_FOLLOWING_POSTS: (useId: string) =>
     `${apiUrl}/social/posts/following/${useId}`, // 動態路由 /:userId
-  GET_PET_POST: `${apiUrl}/social/posts/pet`, // 動態路由 /:petAccount
   GET_SINGLE_POST: `${apiUrl}/social/posts`, // 動態路由 /:postId
   ADD_POST: `${apiUrl}/social/posts`, // 動態路由 /:petId
   DELETE_POST: `${apiUrl}/social/posts/delete`, // 動態路由 /:postId
@@ -33,6 +35,7 @@ const apiBase = {
   // 新增醫療卡片
   ADD_MEDICAL_CARD: (petId: string) =>
     `${apiUrl}/dashboard/cards/medical/add/${petId}`,
+  // 新增重要時刻卡片
   ADD_MOMENT_CARD: (petId: string) =>
     `${apiUrl}/dashboard/cards/moment/add/${petId}`,
 };
