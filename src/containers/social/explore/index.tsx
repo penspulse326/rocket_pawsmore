bad979d798cd2a7a701647f042c5a1a0619be98c
penspulse326 <penspulse@gmail.com>
2024-02-23 13:57:06 +0800

    feat: add PostView to explore

--- src/containers/social/explore/index.tsx
+++ src/containers/social/explore/index.tsx
@@ -1,117 +1,122 @@
-import { PostDataType } from "@/types";
 import Image from "next/image";
+import { useState } from "react";
 
-const postData = [
-  {
-    id: 1,
-    photoUrl: "/test/exp-dog-1.jpg",
-  },
-  {
-    id: 2,
-    photoUrl: "/test/exp-dog-2.jpg",
-  },
-  {
-    id: 3,
-    photoUrl: "/test/exp-dog-3.jpg",
-  },
-  {
-    id: 4,
-    photoUrl: "/test/exp-dog-4.jpg",
-  },
-  {
-    id: 5,
-    photoUrl: "/test/exp-dog-1.jpg",
-  },
-  {
-    id: 6,
-    photoUrl: "/test/exp-dog-2.jpg",
-  },
-  {
-    id: 7,
-    photoUrl: "/test/exp-dog-3.jpg",
-  },
-  {
-    id: 8,
-    photoUrl: "/test/exp-dog-4.jpg",
-  },
-  {
-    id: 9,
-    photoUrl: "/test/exp-dog-1.jpg",
-  },
-  {
-    id: 10,
-    photoUrl: "/test/exp-dog-2.jpg",
-  },
-  {
-    id: 11,
-    photoUrl: "/test/exp-dog-3.jpg",
-  },
-  {
-    id: 12,
-    photoUrl: "/test/exp-dog-4.jpg",
-  },
-];
+import type { PostDataType } from "@/types";
+import { MediaType, SpeciesType } from "@/types/enums";
+import Mask from "@/components/hint/Mask";
+import PostView from "@/components/post/PostView";
+import { useRouter } from "next/router";
 
 interface PropsType {
   posts: PostDataType[];
 }
 
+const speciesData = {
+  [SpeciesType.狗]: {
+    name: "狗",
+    src: "/images/exp-dog.jpg",
+  },
+  [SpeciesType.貓]: {
+    name: "貓",
+    src: "/images/exp-cat.png",
+  },
+  [SpeciesType.倉鼠]: {
+    name: "倉鼠",
+    src: "/images/exp-rice.png",
+  },
+  [SpeciesType.其他]: {
+    name: "其他",
+    src: "/images/exp-more.png",
+  },
+};
+
 const Explore: React.FC<PropsType> = ({ posts }) => {
-  console.log(posts);
+  const router = useRouter();
+  const species: SpeciesType = Number(router.query.species);
+
+  const [isMaskOpen, setIsMaskOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<PostDataType>();
+
+  // 將貼文分成多個批次，每個批次 12 個
+  const batchPosts = (posts: PostDataType[], batchSize: number) => {
+    const batches = [];
+    for (let i = 0; i < posts.length; i += batchSize) {
+      batches.push(posts.slice(i, i + batchSize));
+    }
+    return batches;
+  };
+  // 每批 12 個貼文
+  const postBatches = batchPosts(posts, 12);
+
+  const handleClickPost = (data: PostDataType) => {
+    setIsMaskOpen(!isMaskOpen);
+    setSelectedPost(data);
+  };
+
+  console.log(SpeciesType[0]);
+
   return (
-    <div className="scrollbar-none pt-24 px-8 pb-8 max-w-[658px] w-full border-x border-stroke bg-white overflow-y-scroll">
-      <h1 className="text-[32px]">探索頁面</h1>
-      <Image
-        src="/test/exp-dog.jpg"
-        width={144}
-        height={144}
-        alt="探索頁面"
-        className="my-4 mx-auto rounded-full"
-      />
-      <h2 className="text-2xl text-center">狗</h2>
-      <section className="grid grid-cols-3 gap-2 mt-8">
-        {postData.map((item, index) => (
-          <button
-            type="button"
-            key={`${index}${item.id}`}
-            className={`${
-              (index + 1) % 5 === 0 && index % 3 !== 2
-                ? "col-span-2 row-span-2"
-                : "col-span-1"
-            } rounded-[30px]`}
-          >
-            <Image
-              key={item.id}
-              src={item.photoUrl}
-              width={200}
-              height={200}
-              alt="狗狗"
-              className="w-full h-full rounded-[30px]"
-            />
-          </button>
-        ))}
-        {postData.map((item, index) => (
-          <button
-            type="button"
-            key={`${index}${item.id}`}
-            className={`${
-              (index + 1) % 5 === 0 && index % 3 !== 2
-                ? "col-span-2 row-span-2"
-                : "col-span-1"
-            } rounded-[30px]`}
-          >
-            <Image
-              key={item.id}
-              src={item.photoUrl}
-              width={200}
-              height={200}
-              alt="狗狗"
-              className="w-full h-full rounded-[30px]"
-            />
-          </button>
-        ))}
-      </section>
-    </div>
+    <>
+      {/* 點擊彈出貼文 */}
+      {isMaskOpen && (
+        <Mask maskType="post" setIsOpen={setIsMaskOpen}>
+          <PostView data={selectedPost!} onClose={() => setIsMaskOpen(false)} />
+        </Mask>
+      )}
+      <div className="scrollbar-none pt-24 px-8 pb-8 max-w-[658px] w-full border-x border-stroke bg-white overflow-y-scroll">
+        <h1 className="text-[32px]">探索貼文</h1>
+        <div className="relative mx-auto max-w-[144px] max-h-[144px] w-full h-full rounded-full overflow-hidden">
+          <Image
+            src={speciesData[species]?.src}
+            alt="探索貼文"
+            fill={true}
+            sizes="100%"
+            className="w-auto h-auto object-cover"
+          />
+        </div>
+        <h2 className="mt-4 mb-8 text-2xl text-center">
+          {speciesData[species]?.name}
+        </h2>
+        <section className="flex flex-col gap-2">
+          {postBatches.map((batch, batchIndex) => (
+            <div key={batchIndex} className="grid grid-cols-3 gap-2">
+              {batch.map((post, index) => (
+                <button
+                  type="button"
+                  key={`${batchIndex}-${index}-${post.postId}`}
+                  onClick={() => handleClickPost(post)}
+                  className={`${
+                    (index + 1) % 5 === 0 && index % 3 !== 2
+                      ? "col-span-2 row-span-2"
+                      : "col-span-1"
+                  } rounded-[30px]`}
+                >
+                  <div className="w-full h-full aspect-square rounded-[30px] overflow-hidden">
+                    {post.mediaType === MediaType.image && (
+                      <Image
+                        src={post.media}
+                        width={200}
+                        height={200}
+                        alt={post.petAccount}
+                        className="w-full h-full object-cover"
+                      />
+                    )}
+                    {post.mediaType === MediaType.video && (
+                      <video
+                        src={post.media}
+                        autoPlay={true}
+                        muted={true}
+                        className="w-full h-full bg-black object-cover"
+                      />
+                    )}
+                  </div>
+                </button>
+              ))}
+            </div>
+          ))}
+        </section>
+      </div>
+    </>
   );
 };
 
