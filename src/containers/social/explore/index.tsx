8a0021f704131d34ac2aa633c41e1e67fca925ca
penspulse326 <penspulse@gmail.com>
2024-02-23 11:39:10 +0800

    feat: add get posts by species api

---
+++ src/containers/social/explore/index.tsx
@@ -0,0 +1,118 @@
+import { PostDataType } from "@/types";
+import Image from "next/image";
+
+const postData = [
+  {
+    id: 1,
+    photoUrl: "/test/exp-dog-1.jpg",
+  },
+  {
+    id: 2,
+    photoUrl: "/test/exp-dog-2.jpg",
+  },
+  {
+    id: 3,
+    photoUrl: "/test/exp-dog-3.jpg",
+  },
+  {
+    id: 4,
+    photoUrl: "/test/exp-dog-4.jpg",
+  },
+  {
+    id: 5,
+    photoUrl: "/test/exp-dog-1.jpg",
+  },
+  {
+    id: 6,
+    photoUrl: "/test/exp-dog-2.jpg",
+  },
+  {
+    id: 7,
+    photoUrl: "/test/exp-dog-3.jpg",
+  },
+  {
+    id: 8,
+    photoUrl: "/test/exp-dog-4.jpg",
+  },
+  {
+    id: 9,
+    photoUrl: "/test/exp-dog-1.jpg",
+  },
+  {
+    id: 10,
+    photoUrl: "/test/exp-dog-2.jpg",
+  },
+  {
+    id: 11,
+    photoUrl: "/test/exp-dog-3.jpg",
+  },
+  {
+    id: 12,
+    photoUrl: "/test/exp-dog-4.jpg",
+  },
+];
+
+interface PropsType {
+  posts: PostDataType[];
+}
+
+const Explore: React.FC<PropsType> = ({ posts }) => {
+  console.log(posts);
+  return (
+    <div className="scrollbar-none pt-24 px-8 pb-8 max-w-[658px] w-full border-x border-stroke bg-white overflow-y-scroll">
+      <h1 className="text-[32px]">探索頁面</h1>
+      <Image
+        src="/test/exp-dog.jpg"
+        width={144}
+        height={144}
+        alt="探索頁面"
+        className="my-4 mx-auto rounded-full"
+      />
+      <h2 className="text-2xl text-center">狗</h2>
+      <section className="grid grid-cols-3 gap-2 mt-8">
+        {postData.map((item, index) => (
+          <button
+            type="button"
+            key={`${index}${item.id}`}
+            className={`${
+              (index + 1) % 5 === 0 && index % 3 !== 2
+                ? "col-span-2 row-span-2"
+                : "col-span-1"
+            } rounded-[30px]`}
+          >
+            <Image
+              key={item.id}
+              src={item.photoUrl}
+              width={200}
+              height={200}
+              alt="狗狗"
+              className="w-full h-full rounded-[30px]"
+            />
+          </button>
+        ))}
+        {postData.map((item, index) => (
+          <button
+            type="button"
+            key={`${index}${item.id}`}
+            className={`${
+              (index + 1) % 5 === 0 && index % 3 !== 2
+                ? "col-span-2 row-span-2"
+                : "col-span-1"
+            } rounded-[30px]`}
+          >
+            <Image
+              key={item.id}
+              src={item.photoUrl}
+              width={200}
+              height={200}
+              alt="狗狗"
+              className="w-full h-full rounded-[30px]"
+            />
+          </button>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Explore;
---
+++ src/pages/api/post/species/[species].ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import apiBase from "../../apiBase";
+
+interface ResponseType {
+  statusCode?: number;
+  message: string;
+  data?: any;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { species } = req.query;
+
+  try {
+    const response = await fetch(apiBase.GET_SPECIES_POSTS(species as string), {
+      method: "GET",
+    });
+
+    const result = await response.json();
+    const { statusCode, message, data } = result;
+
+    res.status(statusCode).json({ message, data });
+  } catch (error) {
+    res.status(500).json({ message: "未知的錯誤" });
+  }
+}
