c4f8505b1f71f234a04b40a852c7f2f532d21690
Finn Mao <loverainys@gmail.com>
2024-02-23 21:31:53 +0800

    feat: update route to add pet page

--- src/components/AddPet.tsx
+++ src/components/AddPet.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const AddPet: React.FC = () => {
   return (
-    <div className="flex flex-col gap-y-4 items-center border border-stroke rounded-[30px] p-4 max-w-[224px] w-full">
+    <Link
+      className="flex flex-col gap-y-4 items-center border border-stroke rounded-[30px] p-4 max-w-[224px] w-full"
+      href="/user_dashboard?to=add_pet"
+    >
       <Image
         src="/images/default-photo.png"
         width={192}
@@ -15,7 +19,7 @@ const AddPet: React.FC = () => {
       <button className="bg-primary text-white px-[38px] py-2 rounded-full mb-4">
         新增寵物檔案
       </button>
-    </div>
+    </Link>
   );
 };
 
