806fbee5df3ab66677bd5b7f31022af50e3ae5ac
Finn Mao <loverainys@gmail.com>
2024-02-24 13:44:14 +0800

    feat: modify card content based on param type

--- src/components/AddPet.tsx
+++ src/components/AddPet.tsx
@@ -2,24 +2,42 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const AddPet: React.FC = () => {
+interface PropType {
+  type: "already" | "yet";
+}
+
+interface ContentDataType {
+  type: string;
+  CONTENT: string;
+}
+
+const AddPet: React.FC<PropType> = ({ type }) => {
+  const content: ContentDataType[] = [
+    { type: "already", CONTENT: "有新寵物？" },
+    { type: "yet", CONTENT: "尚未有寵物資料" },
+  ];
+  const selectedContent = content.find((item) => item.type === type);
+
   return (
-    <Link
-      className="flex flex-col gap-y-4 items-center border border-stroke rounded-[30px] p-4 max-w-[224px] w-full"
-      href="/user_dashboard?to=add_pet"
-    >
-      <Image
-        src="/images/default-photo.png"
-        width={192}
-        height={192}
-        className="rounded-[30px]"
-        alt="add pet profile"
-      />
-      <div>尚未有寵物資料</div>
-      <button className="bg-primary text-white px-[38px] py-2 rounded-full mb-4">
+    <div className="flex flex-col min-h-[346px] h-full justify-between items-center bg-white border border-stroke rounded-[30px] p-4 max-w-[224px] w-full">
+      <div className="w-[192px] h-[192px]">
+        <Image
+          src="/images/default-photo.png"
+          width={192}
+          height={192}
+          className="w-full h-full rounded-[30px]"
+          alt="add pet profile"
+        />
+      </div>
+      <div>{selectedContent?.CONTENT}</div>
+      <Link
+        className="bg-primary text-white px-[38px] py-2 rounded-full mb-4"
+        href="/user_dashboard?to=create_pet"
+        shallow={true}
+      >
         新增寵物檔案
-      </button>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
