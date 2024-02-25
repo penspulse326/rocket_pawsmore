6c1baa8701dd66e023fcf42ef4a6a16d557963b3
Finn Mao <loverainys@gmail.com>
2024-02-26 01:32:10 +0800

    feat: use petRecord slice and add NoContent component

--- src/containers/overview/RecentCondition.tsx
+++ src/containers/overview/RecentCondition.tsx
@@ -1,37 +1,48 @@
-import { DailyCardDataType } from "@/types";
-import Daily from "../recordCard/content/Daily";
-import Dot from "@/components/icon/Dot";
-import { PooType, RecordCardType, UrineType, VomitType } from "@/types/enums";
+import { useSelector } from "react-redux";
 import moment from "moment";
-import { formatSymptom } from "@/common/helpers/formatMedicalData";
 
-const data: DailyCardDataType = {
-  cardId: 45,
-  card: 0,
-  weight: "4.kg",
-  food: '[{"type":"濕食","amount":300}]',
-  water: 100,
-  urine: 1,
-  poo: 1,
-  vomit: 5,
-  symptom: '["腹部脹氣","其他"]',
-  deworming: "全能貓",
-  medicine: "內服藥",
-  injection: "皮下",
-  rehab: "按摩",
-  remark: "你怎麼這麼會吃",
-  selected:
-    '["urine","poo","vomit","symptom","deworming","medicine","injection","rehab"]',
-  targetDate: "2024-02-20T00:00:00",
-  createDate: "2024-02-20T13:43:02.02",
-};
+import NoContent from "@/components/NoContent";
+
+import { formatSymptom } from "@/common/helpers/formatMedicalData";
+import getPetAge from "@/common/helpers/getPetAge";
+
+import { RootState } from "@/common/redux/store";
+import { DailyCardDataType } from "@/types";
+import { PooType, UrineType, VomitType } from "@/types/enums";
 
 const RecentCondition: React.FC = () => {
+  const petList = useSelector((state: RootState) => state.petList);
+  const petRecord = useSelector((state: RootState) => state.petRecord);
+
+  const selectedPet = petList.find((pet) => pet.petId === petRecord.petId);
+
+  const currentRecord = petRecord.data.find((event) => {
+    const { card, urine, poo, vomit, symptom } = event as DailyCardDataType;
+    return (
+      card === 0 &&
+      (urine !== 0 || poo !== 0 || vomit !== 0 || symptom !== "[]")
+    );
+  });
+
+  if (!currentRecord) {
+    return (
+      <section className="flex flex-col gap-y-4 scrollbar-none max-w-[497px] max-h-[386px] w-full">
+        <h2 className="text-2xl leading-9">近期異常狀況</h2>
+        <div className="mt4 px-6 py-4 h-[335px] border border-stroke rounded-[30px]">
+          <NoContent />
+        </div>
+      </section>
+    );
+  }
+
+  const { urine, poo, vomit, symptom, targetDate, remark } =
+    currentRecord as DailyCardDataType;
+
   const sickList = [
-    { name: "尿液", value: UrineType[data.urine] },
-    { name: "糞便", value: PooType[data.poo] },
-    { name: "嘔吐", value: VomitType[data.vomit] },
-    { name: "症狀", value: formatSymptom(data.symptom) },
+    { name: "尿液", value: UrineType[urine] },
+    { name: "糞便", value: PooType[poo] },
+    { name: "嘔吐", value: VomitType[vomit] },
+    { name: "症狀", value: formatSymptom(symptom) },
   ];
 
   return (
@@ -44,10 +55,10 @@ const RecentCondition: React.FC = () => {
             <span className="ml-1 font-bold">日常紀錄</span>
             {/* 卡片時間 */}
             <span className="ml-2 text-note">
-              {moment(data.targetDate).format("YYYY/MM/DD")}
+              {moment(targetDate).format("YYYY/M/D")}
             </span>
           </div>
-          <span className="text-note">1 歲 3 個月</span>
+          <span className="text-note">{getPetAge(selectedPet?.birthday!)}</span>
         </div>
         <ul className="flex flex-col gap-2 mt-6">
           <li className="text-note">異常</li>
@@ -63,7 +74,7 @@ const RecentCondition: React.FC = () => {
         <hr className="mt-2" />
         <div className="mt-4">
           <span className="text-note">備註</span>
-          <p className="mt-2">{data.remark}</p>
+          <p className="mt-2">{remark}</p>
         </div>
       </div>
     </section>
