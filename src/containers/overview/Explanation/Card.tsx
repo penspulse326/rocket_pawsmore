43528fa4441534e04b51aa12b5ce8a6cda17667d
Finn Mao <loverainys@gmail.com>
2024-02-26 16:48:11 +0800

    feat: show current abnormal condition details

--- src/containers/overview/Explanation/Card.tsx
+++ src/containers/overview/Explanation/Card.tsx
@@ -1,14 +1,27 @@
-import { UrineType } from "@/types/enums";
+import { symptomInfo } from "@/common/lib/symptomInfo";
+import { UrineType, PooType, VomitType } from "@/types/enums";
 
-const data = {
-  sick: {
-    name: "尿液",
-    type: UrineType[UrineType.紅色或粉紅色],
-    reason: "感染、結石、膀胱炎、前列腺炎等導致。",
-  },
-};
+interface Symptom {
+  urine?: number;
+  poo?: number;
+  vomit?: number;
+  symptom?: string;
+}
 
-const Card: React.FC = () => {
+interface CardProps {
+  data: Symptom;
+}
+
+const Card: React.FC<CardProps> = ({ data }) => {
+  const { urine, poo, vomit, symptom } = data;
+
+  const urineType = symptomInfo.urine.find((item) => item.type === urine)?.type;
+  const pooType = symptomInfo.poo.find((item) => item.type === poo)?.type;
+  const vomitType = symptomInfo.vomit.find((item) => item.type === vomit)?.type;
+  const symptomType = symptomInfo.symptom.find(
+    (item) => item.type === symptom
+  )?.type;
+
   return (
     <li className="flex flex-col gap-4 px-6 py-4 border border-stroke rounded-[30px]">
       <div>
@@ -16,8 +29,12 @@ const Card: React.FC = () => {
           異常狀況
         </span>
         <div className="flex gap-2 mt-1">
-          <span>{data.sick.name}</span>
-          <span>{data.sick.type}</span>
+          <span>
+            {urineType !== undefined && UrineType[urineType]}
+            {pooType !== undefined && PooType[pooType]}
+            {vomitType !== undefined && VomitType[vomitType]}
+            {symptomType !== undefined && symptomType}
+          </span>
         </div>
       </div>
       <div>
@@ -25,7 +42,26 @@ const Card: React.FC = () => {
           可能原因
         </span>
         <div className="flex gap-2 mt-1">
-          <span>{data.sick.reason}</span>
+          {urine && (
+            <span>
+              {symptomInfo.urine.find((item) => item.type === urine)?.text}
+            </span>
+          )}
+          {poo && (
+            <span>
+              {symptomInfo.poo.find((item) => item.type === poo)?.text}
+            </span>
+          )}
+          {vomit && (
+            <span>
+              {symptomInfo.vomit.find((item) => item.type === vomit)?.text}
+            </span>
+          )}
+          {symptom && (
+            <span>
+              {symptomInfo.symptom.find((item) => item.type === symptom)?.text}
+            </span>
+          )}
         </div>
       </div>
     </li>
