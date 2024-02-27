f9787647b9b0efeb5403c02ca21c69979086fdfe
SHIN <akiyachiumi@gmail.com>
2024-02-28 07:19:46 +0800

    fixed: fix delete post's params type

--- src/components/post/PostMenu.tsx
+++ src/components/post/PostMenu.tsx
@@ -5,6 +5,7 @@ import { MediaType } from "@/types/enums";
 import { IconDotsVertical } from "@tabler/icons-react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import Loading from "../hint/Loading";
 
 interface PropsType {
   postId: number;
@@ -25,49 +26,62 @@ const Menu: React.FC<PropsType> = ({
 }) => {
   const { token } = useToken();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleDeletePost = async () => {
     if (!token) {
       alert("請先登入");
       return;
     }
-    await fetchDeletePost(token, postId, media, MediaType[mediaType]);
+    setIsLoading(true);
+
+    await fetchDeletePost(
+      token,
+      postId,
+      media,
+      MediaType[mediaType] as "image" | "video"
+    );
     getList();
     handleClose();
+
+    setIsLoading(false);
   };
 
   return (
-    <div
-      onClick={() => setIsMenuOpen(!isMenuOpen)}
-      className="relative cursor-pointer"
-    >
-      <IconDotsVertical />
-      {isMenuOpen && (
-        <ul className="absolute right-0 mt-2 p-3 w-28 rounded-3xl bg-white shadow-custom">
-          <li>
-            <button type="button" className="px-3 py-1">
-              複製連結
-            </button>
-          </li>
-          <li>
-            <button type="button" className="px-3 py-1 text-error">
-              檢舉貼文
-            </button>
-          </li>
-          {isAuthor && (
+    <>
+      <div
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="relative cursor-pointer"
+      >
+        <IconDotsVertical />
+        {isMenuOpen && (
+          <ul className="absolute right-0 mt-2 p-3 w-28 rounded-3xl bg-white shadow-custom">
             <li>
-              <button
-                type="button"
-                onClick={handleDeletePost}
-                className="px-3 py-1 text-error"
-              >
-                刪除貼文
+              <button type="button" className="px-3 py-1">
+                複製連結
               </button>
             </li>
-          )}
-        </ul>
-      )}
-    </div>
+            <li>
+              <button type="button" className="px-3 py-1 text-error">
+                檢舉貼文
+              </button>
+            </li>
+            {isAuthor && (
+              <li>
+                <button
+                  type="button"
+                  onClick={handleDeletePost}
+                  className="px-3 py-1 text-error"
+                >
+                  刪除貼文
+                </button>
+              </li>
+            )}
+          </ul>
+        )}
+      </div>
+      {isLoading && <Loading />}
+    </>
   );
 };
 
