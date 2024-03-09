d9d59dce6a477ea2c7c46e96d10a65aac79da038
SHIN <akiyachiumi@gmail.com>
2024-03-09 08:45:23 +0800

    chore: install eslint-import-resolver-alias

--- src/containers/auth/Layout.tsx
+++ src/containers/auth/Layout.tsx
@@ -1,48 +1,50 @@
 import Footer from '@/components/Footer';
 import Image from 'next/image';
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <section className='outer'>
-    <div className='inner flex h-screen flex-col'>
-      <section className='grid flex-grow grid-cols-12 gap-8'>
-        {/* 圖片 */}
-        <header className='col-span-6 flex flex-col justify-center gap-12 pl-12'>
-          <div className='flex items-center gap-4'>
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className='outer'>
+      <div className='inner flex h-screen flex-col'>
+        <section className='grid flex-grow grid-cols-12 gap-8'>
+          {/* 圖片 */}
+          <header className='col-span-6 flex flex-col justify-center gap-12 pl-12'>
+            <div className='flex items-center gap-4'>
+              <Image
+                src='/images/logo-rounded.svg'
+                alt='logo'
+                priority
+                width={82}
+                height={82}
+              />
+              <Image
+                src='/images/logo-text.svg'
+                alt='logo-text'
+                priority
+                width={297}
+                height={62}
+                className='h-auto w-auto'
+              />
+            </div>
             <Image
-              src='/images/logo-rounded.svg'
-              alt='logo'
-              priority={true}
-              width={82}
-              height={82}
-            />
-            <Image
-              src='/images/logo-text.svg'
-              alt='logo-text'
-              priority={true}
-              width={297}
-              height={62}
+              src='/images/banner-home.png'
+              alt='home-banner'
+              priority
+              width={563}
+              height={70}
               className='h-auto w-auto'
             />
+          </header>
+          {/* 表單 */}
+          <div className='col-span-5 col-start-8 flex flex-col justify-center pr-12'>
+            <section className='flex flex-col justify-center gap-8 rounded-[30px] border border-stroke p-8'>
+              {children}
+            </section>
           </div>
-          <Image
-            src='/images/banner-home.png'
-            alt='home-banner'
-            priority={true}
-            width={563}
-            height={70}
-            className='h-auto w-auto'
-          />
-        </header>
-        {/* 表單*/}
-        <div className='col-span-5 col-start-8 flex flex-col justify-center pr-12'>
-          <section className='flex flex-col justify-center gap-8 rounded-[30px] border border-stroke p-8'>
-            {children}
-          </section>
-        </div>
-      </section>
-    </div>
-    <Footer />
-  </section>
-);
+        </section>
+      </div>
+      <Footer />
+    </section>
+  );
+}
 
 export default Layout;
--- src/containers/auth/Login.tsx
+++ src/containers/auth/Login.tsx
@@ -1,16 +1,16 @@
-import { Controller, useForm } from "react-hook-form";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { setUserInfo } from "@/common/redux/userInfoSlice";
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Controller, useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { fetchLogin } from '@/common/fetch/auth';
+import useToken from '@/common/hooks/useToken';
 
-import Loading from "@/components/hint/Loading";
-import TextInput from "@/components/form/profile/TextInput";
-import PasswordInput from "@/components/form/profile/PasswordInput";
-import { errorText } from "@/common/lib/messageText";
-import { fetchLogin } from "@/common/fetch/auth";
-import useToken from "@/common/hooks/useToken";
+import { errorText } from '@/common/lib/messageText';
+import { setUserInfo } from '@/common/redux/userInfoSlice';
+import PasswordInput from '@/components/form/profile/PasswordInput';
+import TextInput from '@/components/form/profile/TextInput';
+import Loading from '@/components/hint/Loading';
 
 interface LoginFormType {
   email: string;
@@ -43,10 +43,10 @@ const Login: React.FC = () => {
       dispatch(setUserInfo(response.data));
       updateUser(token, userId);
       if (!username) {
-        router.push("/member/new/profile");
+        router.push('/member/new/profile');
         return;
       }
-      router.push("/");
+      router.push('/');
     }
 
     setStatusCode(response.status);
@@ -58,13 +58,13 @@ const Login: React.FC = () => {
   useEffect(() => {
     switch (statusCode) {
       case 400:
-        setError("email", { message: errorText.EMAIL_NOT_EXIST });
+        setError('email', { message: errorText.EMAIL_NOT_EXIST });
         break;
       case 401:
-        setError("email", { message: errorText.ALL_NOT_MATCH });
+        setError('email', { message: errorText.ALL_NOT_MATCH });
         break;
       case 500:
-        setError("email", { message: errorText.UNKNOWN_ERROR });
+        setError('email', { message: errorText.UNKNOWN_ERROR });
         break;
       default:
         break;
@@ -73,14 +73,14 @@ const Login: React.FC = () => {
 
   return (
     <>
-      <div className="flex flex-col gap-y-1">
-        <h2 className="text-[32px]">歡迎回來！</h2>
-        <h3 className="text-note">讓我們繼續記錄美好時光！</h3>
+      <div className='flex flex-col gap-y-1'>
+        <h2 className='text-[32px]'>歡迎回來！</h2>
+        <h3 className='text-note'>讓我們繼續記錄美好時光！</h3>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
         {/* Email */}
         <Controller
-          name="email"
+          name='email'
           control={control}
           rules={{
             required: errorText.REQUIRED,
@@ -91,17 +91,17 @@ const Login: React.FC = () => {
           }}
           render={({ field }) => (
             <TextInput
-              title="Email"
-              placeholder="輸入電子郵件地址"
+              title='Email'
+              placeholder='輸入電子郵件地址'
               message={errors.email?.message}
               {...field}
             />
           )}
         />
         {/* 密碼 */}
-        <div className="flex flex-col gap-1">
+        <div className='flex flex-col gap-1'>
           <Controller
-            name="password"
+            name='password'
             control={control}
             rules={{
               required: errorText.REQUIRED,
@@ -112,30 +112,30 @@ const Login: React.FC = () => {
             }}
             render={({ field }) => (
               <PasswordInput
-                title="密碼"
-                placeholder="輸入密碼"
+                title='密碼'
+                placeholder='輸入密碼'
                 message={errors.password?.message}
                 {...field}
               />
             )}
           />
-          <Link href="#" className="self-end text-primary">
+          <Link href='#' className='self-end text-primary'>
             忘記密碼？
           </Link>
         </div>
         <button
-          type="submit"
+          type='submit'
           disabled={isBtnDisabled}
           className={`${
-            isValid ? "bg-primary" : "bg-note"
-          } mt-4 py-3 rounded-full  text-white`}
+            isValid ? 'bg-primary' : 'bg-note'
+          } mt-4 rounded-full py-3  text-white`}
         >
           登入
         </button>
       </form>
-      <div className="flex justify-center gap-4">
+      <div className='flex justify-center gap-4'>
         還沒有帳號？
-        <Link href="/signup" className="text-primary underline">
+        <Link href='/signup' className='text-primary underline'>
           立刻加入
         </Link>
       </div>
