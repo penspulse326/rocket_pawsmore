d0056591fdad926ac83a9c82e34d0262e4300da8
penspulse326 <penspulse@gmail.com>
2024-03-13 15:20:56 +0800

    style: fix _app and _document's lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,37 +2,20 @@ import '@/styles/globals.css';
 import '@/styles/animations.css';
 import '@/styles/loadings.css';
 
-import type { ReactElement, ReactNode } from 'react';
-import moment from 'moment';
-import 'moment/locale/zh-tw';
+import { AppProps } from 'next/app';
 import { Noto_Sans_TC } from 'next/font/google';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
+
 import { store } from '@/common/redux/store';
-
 import Navbar from '@/components/navbar/Navbar';
-
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
-
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
 
 const notos = Noto_Sans_TC({
   weight: ['400', '600', '700'],
   subsets: ['latin'],
 });
 
-moment.locale('zh-tw');
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
@@ -43,7 +26,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       <Provider store={store}>
         <main className={notos.className}>
           <Navbar />
-          {getLayout(<Component {...pageProps} />)}
+          <Component {...pageProps} />
         </main>
       </Provider>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSidePropsContext } from 'next';
+import type { GetServerSidePropsContext } from 'next';
 
 import cookie from 'cookie';
 
