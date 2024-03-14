30771554e171735046832ff3cd3e9c672bb8177e
SHIN <akiyachiumi@gmail.com>
2024-03-14 19:16:24 +0800

    chore: add Mask's directions

--- src/components/hint/Mask.tsx
+++ src/components/hint/Mask.tsx
@@ -9,6 +9,7 @@ function Mask({ onClose, children }: PropsType) {
   // 記憶當前 scrollbar 位置
   const scrollbarPosition = useRef(window.scrollY);
 
+  // 觸發滾動事件時重新設定 scrollbar 位置
   const handleScroll = () => {
     window.scrollTo(0, scrollbarPosition.current);
   };
@@ -16,6 +17,7 @@ function Mask({ onClose, children }: PropsType) {
   const handleClose = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
 
+    // 點擊到有 mask 的元素時關閉
     const target = event.target as HTMLElement;
     if (target.classList.contains('mask')) {
       onClose();
